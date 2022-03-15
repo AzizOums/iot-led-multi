8c84ca6448051679808855de15a7c3b5424d3b77
Aziz OUMSALEM <aziz.oumsalem@outlook.fr>
2022-03-16 00:57:55 +0100

    feat: add color hexadecimal mqtt controle

--- led_multi/led_multi.ino
+++ led_multi/led_multi.ino
@@ -1,174 +1,232 @@
+#include <Adafruit_NeoPixel.h>
+#include <EspMQTTClient.h>
 #include <time.h>
 
-#include "EspMQTTClient.h"
-#include <Adafruit_NeoPixel.h>
-
-#define PIN       18
+#define PIN 18
 #define NUMPIXELS 16
-#define DELAYVAL  500
+#define DELAYVAL 500
 #define MAXPLANIF 7
 
 time_t now;
 time_t last;
-struct tm* date;
-const char* ntpServer = "pool.ntp.org";
+struct tm *date;
+
+const char *ntpServer = "pool.ntp.org";
+const String mqttServer = "test.mosquitto.org";
+const String wifiSsid = "Aziz";
+const String wifiPwd = "Aziz1998";
 
 String planif[MAXPLANIF];
 int nbPlanif = 0;
 int indexPlanif = 0;
 
 boolean initialised = false;
+int rgb[3];
 int brightness = 180;
 int color = 0;
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 EspMQTTClient client("Aziz", "Aziz1998", "test.mosquitto.org");
 
-void mqttSetup() {
+void mqttSetup()
+{
   client.enableDebuggingMessages();
   client.enableHTTPWebUpdater();
   client.enableOTA();
 }
 
-void ledControle(String payload) {
-  int c = -1;
-  if(payload == "0") c = 0;
-  else if(payload == "1") c = 1;
-  else if(payload == "2") c = 2;
-  else if(payload == "3") c = 3;
-  else if(payload == "4") c = 4;
-  if(c != -1 && c != color) {
-    setState(c);
-    color = c;
+void ledControle(String payload)
+{
+  if (payload.startsWith("#") && payload.length() == 7)
+    setColor(payload);
+  else
+  {
+    int c = payload.toInt();
+    if (c != color && c >= 0 && c <= 4)
+    {
+      setState(c);
+      color = c;
+    }
   }
 }
 
-void brightnessControle(String payload) {
-  if(payload) {
+void brightnessControle(String payload)
+{
+  if (payload)
+  {
     int b = payload.toInt();
-    if(b > 0 && b <= 255) changeBrightness(b);
+    if (b > 0 && b <= 255)
+      changeBrightness(b);
   }
 }
 
-void planifControle(String payload) {
-  if(payload.length() == 13) {
+void planifControle(String payload)
+{
+  if (payload.length() == 13)
+  {
     int i = indexPlanif % MAXPLANIF;
     planif[i] = payload;
-    if(nbPlanif < MAXPLANIF) nbPlanif++;
+    if (nbPlanif < MAXPLANIF)
+      nbPlanif++;
     indexPlanif = i + 1;
   }
 }
 
-void mqttControle() {
-  client.subscribe("iot/led", [](const String & payload) {
-    ledControle(payload);
-  });
-  client.subscribe("iot/brightness", [](const String & payload) {
-    brightnessControle(payload);
-  });
-  client.subscribe("iot/planification", [](const String & payload) {
-    planifControle(payload);
-  });
+void mqttControle()
+{
+  client.subscribe("iot/led", [](const String &payload)
+                   { ledControle(payload); });
+  client.subscribe("iot/brightness", [](const String &payload)
+                   { brightnessControle(payload); });
+  client.subscribe("iot/planification", [](const String &payload)
+                   { planifControle(payload); });
 }
 
-void sendTelemetrie() {
+void sendTelemetrie()
+{
   String s = "\"state\": " + (String)((color != 0 ? 1 : 0));
   String b = "\"brightness\": " + (String)(color != 0 ? brightness : 0);
-  String c = "\"color\": ";
-  switch (color) {
-    case 0: c += "\"None\""; break;
-    case 1: c += "\"Orange\""; break;
-    case 2: c += "\"Purple\""; break;
-    case 3: c += "\"Green\""; break;
-    case 4: c += "\"White\""; break;
-  }
+  String c = "\"color\": \"rgb(" + (String)rgb[0] + ", " + (String)rgb[1] + ", " + (String)rgb[2] + ")\"";
   String msg = "{" + s + ", " + c + ", " + b + "}";
   client.publish("iot/telemetrie", msg);
 }
 
-void listPlanif() {
+void listPlanif()
+{
   String msg = "{ \"nbPlanif\": " + (String)nbPlanif + ", \"planif\": [";
-  for(int i=0; i<nbPlanif; i++) {
+  for (int i = 0; i < nbPlanif; i++)
+  {
     msg += "\"" + planif[i] + "\"";
-    if(i != nbPlanif - 1) msg += ", ";
+    if (i != nbPlanif - 1)
+      msg += ", ";
   }
   msg += "] }";
   client.publish("iot/list-planif", msg);
 }
 
-void onConnectionEstablished() {
+void onConnectionEstablished()
+{
   mqttControle();
 }
 
-void changeColor(int r, int g, int b) {
+void changeColor(int rgbCol[])
+{
   pixels.clear();
-  for(int i=0; i<NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(r, g, b));
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
+    pixels.setPixelColor(i, pixels.Color(rgbCol[0], rgbCol[1], rgbCol[2]));
     pixels.show();
   }
+  if (rgb != rgbCol)
+    rgb[0], rgb[1], rgb[2] = rgbCol[0], rgbCol[1], rgbCol[2];
 }
 
-void changeBrightness(int b) {
+void changeBrightness(int b)
+{
   brightness = b;
   pixels.setBrightness(b);
   pixels.show();
 }
 
-void setPurple() {
-  changeColor(255,0,255);
+void setColor(String hex)
+{
+  char x[3];
+  hex.substring(1, 3).toCharArray(x, 3);
+  rgb[0] = (int)strtol(x, NULL, 16);
+  hex.substring(3, 5).toCharArray(x, 3);
+  rgb[1] = (int)strtol(x, NULL, 16);
+  hex.substring(5, 7).toCharArray(x, 3);
+  rgb[2] = (int)strtol(x, NULL, 16);
+  changeColor(rgb);
 }
 
-void setGreen() {
-  changeColor(173,255,47);
+// couleurs prédéfinies
+void setPurple()
+{
+  int tmp[] = {255, 0, 255};
+  changeColor(tmp);
 }
 
-void setOrange() {
-  changeColor(255,69,0);
+void setGreen()
+{
+  int tmp[] = {173, 255, 47};
+  changeColor(tmp);
 }
 
-void setWhite() {
-  changeColor(248,248,255);
+void setOrange()
+{
+  int tmp[] = {255, 69, 0};
+  changeColor(tmp);
 }
 
-void shutDown() {
+void setWhite()
+{
+  int tmp[] = {248, 248, 255};
+  changeColor(tmp);
+}
+
+// led off
+void shutDown()
+{
   pixels.clear();
   pixels.show();
 }
 
-void setState(int c) {
-  switch (c % 5) {
-    case 0: shutDown(); break;
-    case 1: setOrange(); break;
-    case 2: setPurple(); break;
-    case 3: setGreen(); break;
-    case 4: setWhite(); break;
+// mettre l'une des couleurs prédéfinies
+void setState(int c)
+{
+  switch (c % 5)
+  {
+  case 0:
+    shutDown();
+    break;
+  case 1:
+    setOrange();
+    break;
+  case 2:
+    setPurple();
+    break;
+  case 3:
+    setGreen();
+    break;
+  case 4:
+    setWhite();
+    break;
   }
 }
 
-void touchControle() {
-  if (touchRead(13) < 10) {
-    color ++;
+// capteur de toucher
+void touchControle()
+{
+  if (touchRead(13) < 10)
+  {
+    color++;
     setState(color);
-    if(color % 5 == 0) color = 0;
-    if(!initialised) initialised = true;
+    if (color % 5 == 0)
+      color = 0;
+    if (!initialised)
+      initialised = true;
   }
 }
 
-void getTime() {
+void getTime()
+{
   time_t timestamp = time(NULL);
   ctime(&timestamp);
   now = timestamp;
   date = localtime(&now);
 }
 
-// s doit être du format: D HH:MM C BBB
-void compareTime(String s) {
+// s: D HH:MM C BBB
+void compareTime(String s)
+{
   int d = s.substring(0, 1).toInt();
   int h = s.substring(2, 4).toInt();
   int m = s.substring(5, 7).toInt();
   int c = s.substring(8, 9).toInt();
   int b = s.substring(10, s.length()).toInt();
-  if(date->tm_wday == d && date->tm_hour == h && date->tm_min == m && date->tm_sec >= 0 && date->tm_sec < 3) {
+  if (date->tm_wday == d && date->tm_hour == h && date->tm_min == m && date->tm_sec >= 0 && date->tm_sec < 3)
+  {
     color = c;
     brightness = b;
     setState(color);
@@ -176,13 +234,16 @@ void compareTime(String s) {
   }
 }
 
-void comparePlanif() {
-  for(int i=0; i<nbPlanif; i++) {
+void comparePlanif()
+{
+  for (int i = 0; i < nbPlanif; i++)
+  {
     compareTime(planif[i]);
   }
 }
 
-void setup() {
+void setup()
+{
   configTzTime("CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00", ntpServer);
   Serial.begin(115200);
   pixels.begin();
@@ -190,18 +251,22 @@ void setup() {
   setState(0);
 }
 
-void loop() {
+void loop()
+{
   getTime();
-  if(initialised) {
+  if (initialised)
+  {
     client.loop();
-    if (now - last >= 10) {
+    if (now - last >= 10)
+    {
       sendTelemetrie();
       listPlanif();
       last = now;
     }
   }
-  if(!client.isConnected())
+  if (!client.isConnected())
     touchControle();
-  if(nbPlanif > 0) comparePlanif();
+  if (nbPlanif > 0)
+    comparePlanif();
   delay(DELAYVAL);
 }
